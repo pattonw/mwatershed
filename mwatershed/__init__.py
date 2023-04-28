0ab08a19d03dc0c497f6c621dfab2c0997aac14e
pattonw <pattonw@hhmi.org>
2023-04-28 10:54:39 -0700

    add cluster to init

--- mwatershed/__init__.py
+++ mwatershed/__init__.py
@@ -6,6 +6,6 @@ __email__ = """pattonw@hhmi.org"""
 __version__ = """0.1.0"""
 __version_info__ = tuple(int(n) for n in __version__.split('.'))
 
-from .mwatershed import agglom
+from .mwatershed import agglom, cluster
 
-__all__ = ["agglom"]
+__all__ = ["agglom", "cluster"]
