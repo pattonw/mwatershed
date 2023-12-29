fd857b3edd00146bb9bc90579306761f0d6a9ff5
William Patton <wllmpttn24@gmail.com>
2023-12-29 12:28:45 -0500

    wrap agglom function with type hints

--- mwatershed/__init__.py
+++ mwatershed/__init__.py
@@ -6,6 +6,18 @@ __email__ = """pattonw@hhmi.org"""
 __version__ = """0.1.0"""
 __version_info__ = tuple(int(n) for n in __version__.split("."))
 
-from .mwatershed import agglom, cluster
+from .mwatershed import agglom_rs, cluster
+import numpy as np
+from typing import Optional
+
+
+def agglom(
+    affinities: np.ndarray,
+    offsets: list[list[int]],
+    seeds: Optional[np.ndarray] = None,
+    edges: Optional[list[tuple[bool, int, int]]] = None,
+):
+    return agglom_rs(affinities, offsets, seeds, edges)
+
 
 __all__ = ["agglom", "cluster"]
