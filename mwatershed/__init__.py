6d2212dd8b6145f2478eaffaa60b5163d177ab9a
William Patton <wllmpttn24@gmail.com>
2023-12-29 14:13:17 -0500

    fix version in __init__.py

--- mwatershed/__init__.py
+++ mwatershed/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """William Hunter Patton"""
 __email__ = """pattonw@hhmi.org"""
-__version__ = """0.1.0"""
+__version__ = """0.4.0"""
 __version_info__ = tuple(int(n) for n in __version__.split("."))
 
 from .mwatershed import agglom_rs, cluster
